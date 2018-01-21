eb2d1f6e77ce8029126784e6127e2c4ab929f6e4
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2018-01-21 15:48:22 +0100

    funcionalidad se muestra y oculta ficha

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 function toggleMenu() {
     $('#menuToggle').toggleClass('active');
     $('#overlay').toggleClass('open');
+    $('.lista-animales li').removeClass('ficha-activa').addClass('ficha-no-activa');
 }
 $('.menu-toggle, #mainNav a').click(function () {
     toggleMenu();
 });
 
 $(".lista-animales").delegate("li", "click", function(){
-    $(this).addClass('ficha-activa');
+    $(this).removeClass('ficha-no-activa').addClass('ficha-activa');
+    $('body').addClass('overflow-hidden');
 });
+
+$(".cerrar-ficha").click(function (e) {
+    e.stopPropagation();
+    $(this).closest('li').removeClass('ficha-activa').addClass('ficha-no-activa');
+    $('body').removeClass('overflow-hidden');
+})
